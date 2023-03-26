8e12726fe0bdf10bb6e90db189a929174ae7276f
지영 <toto.joung@gmail.com>
2023-03-26 11:33:15 +0900

    파일교체

--- 1-vanilla/js/main.js
+++ 1-vanilla/js/main.js
@@ -1,28 +1,16 @@
 import Controller from "./Controller.js";
 import Store from "./store.js";
 import storage from "./storage.js";
-import SearchFormView from "./views/SearchFormView.js";
-import SearchResultView from "./views/SearchResultView.js";
-import TabView from "./views/TabView.js";
-import KeywordListView from "./views/KeywordListView.js";
-import HistoryListView from "./views/HistoryListView.js";
-
-const tag = "[main]";
+import SearchFormView from "./views/searchFormView.js";
 
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  console.log(tag, "main");
-
   const store = new Store(storage);
 
   const views = {
-    searchFormView: new SearchFormView(),
-    searchResultView: new SearchResultView(),
-    tabView: new TabView(),
-    keywordListView: new KeywordListView(),
-    historyListView: new HistoryListView(),
-  };
+    searcgFormView: new SearchFormView(),
+  }
 
   new Controller(store, views);
 }
