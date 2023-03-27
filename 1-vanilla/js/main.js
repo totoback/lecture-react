24d9e186e0fed5bb099061d73fcb150166977864
지영 <toto.joung@gmail.com>
2023-03-27 09:13:56 +0900

    겅부

--- 1-vanilla/js/main.js
+++ 1-vanilla/js/main.js
@@ -3,9 +3,11 @@ import Store from "./store.js";
 import storage from "./storage.js";
 import SearchFormView from "./views/searchFormView.js";
 
+const tag = "[main]"
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
+  console.log(tag, "main")
   const store = new Store(storage);
 
   const views = {
--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,14 +1,32 @@
 import { qs } from "../helpers.js";
 import View from "./View.js";
 
+const tag = "[SearchFormView]";
+
 export default class SearchFormView extends View {
   constructor() {
-    super(qs("#serch-form-view"));
+    super(qs("#search-form-view"));
 
-    this.resetElement = qs("[type=reset", this.element);
+    this.inputElement = qs("[type=text]", this.element);
+    this.resetElement = qs("[type=reset]", this.element);
+
     this.showResetButton(false);
+    this.bindEvents();
   }
-  showResetButton(visible = ture) {
+  showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+  bindEvents() {
+    on(this.inputElement, "keyup", (event) => this.handleKeyup(event));
+    on(this.element, "submit", (event) => this.handleSubmit(event));
+  }
+  handleKeyup(event) {
+    // console.log(tag, 'handleKeyup', this.inputElement.value);
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(tag, "handleSubmit");
   }
 }
